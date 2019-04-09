3b3ab01298b53ad117394f90296438b66489f60d
saisgs <sai.sgs47@gmail.com>
2019-04-09 11:35:33 -0700

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3b93m1krqsr2vsi3puq878iphd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
